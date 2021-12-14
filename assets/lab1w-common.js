d68a67a34729932c7c9b542a2b11000771dc8564
Yotam Faraggi <yotam@curalife.com>
2021-12-14 15:24:51 +0200

    Tue Dec 14 15:24:51 IST 2021

--- assets/lab1w-common.js
+++ assets/lab1w-common.js
@@ -111,11 +111,6 @@ const dqSlider = new Swiper('.doctors-quote .dq-slider .swiper-container', {
     spaceBetween: 8,
     grabCursor: true,
     loop: false,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-        disabledClass: 'disabled'
-    },
     pagination: {
         el: '.slider-bullets',
         bulletClass: 'bullet',
@@ -352,12 +347,9 @@ window.requestAnimFrame = (function () {
 
     const get_utm = window.location.search.substring(1);
     let product_link = document.querySelectorAll('.product-list .card .submit-btn a, .list-wrap .card .c-wrap .bottom .b-wrap a,\
-    .chose-pack .c-wrap .pack-link .active-link a, .chose-pack .c-wrap .pack-link .link a, .product-list  .single-card .main-info .btn-wrap .main-btn,\
-    .product-list line-decoration .card .bottom .submit-btn a, .product-list .card .submit-btn a,\
-    .more-space.v3 .product-list .single-card .main-info .btn-wrap .main-btn, .product-list-lumen .single-card .main-info .btn-wrap .main-btn,\
-    .product-list-lumen .single-card .main-info .btn-wrap-b .main-btn, .goods-list-face .list-wrap .face-card .submit-btn a,\
-    .goods-list-face .list-wrap-face .face-card-2 .submit-btn a, .goods-list .list-wrap .sale-card .submit-btn a,\
-    .goods-list.sale .list-wrap .card .submit-btn a');
+     .chose-pack .c-wrap .pack-link .active-link a, .chose-pack .c-wrap .pack-link .link a, .product-list  .single-card .main-info .btn-wrap .main-btn,\
+     .product-list line-decoration .card .bottom .submit-btn a, .product-list .card .submit-btn a,\
+     .more-space.v3 .product-list .single-card .main-info .btn-wrap .main-btn'); 
     let floating_btn_wrap = document.querySelector('.about-content');
     let floating_btn = document.querySelector('.about-content .floating-btn');
     let cta_floating_btn = document.querySelector('.article-content .cta-btn');
@@ -376,10 +368,7 @@ window.requestAnimFrame = (function () {
         let ticker_list = document.querySelectorAll('.seen-headlines .headlines-list .ticker-wrap ul');
         for (let i = 0; i < ticker_list.length; i++) {
             let _clone = ticker_list[i].cloneNode(true);
-            if(ticker[i]) {
-                ticker[i].append(_clone)
-            }
-
+            ticker[i].append(_clone)
         }
     }
 
@@ -387,23 +376,12 @@ window.requestAnimFrame = (function () {
         let last_known_scroll_position = window.scrollY;
 
         if (floating_btn) {
-            if (document.querySelector('.about-content .product-list')) {
-                if (floating_btn_wrap.offsetTop < last_known_scroll_position &&
-                    document.querySelector('.about-content .product-list').offsetTop > last_known_scroll_position) {
-                    floating_btn.classList.add('show');
-                } else {
-                    floating_btn.classList.remove('show');
-                }
+            if (floating_btn_wrap.offsetTop < last_known_scroll_position &&
+                document.querySelector('.about-content .product-list').offsetTop > last_known_scroll_position) {
+                floating_btn.classList.add('show');
+            } else {
+                floating_btn.classList.remove('show');
             }
-            if (document.querySelector('.about-content .goods-list')) {
-                if (floating_btn_wrap.offsetTop < last_known_scroll_position &&
-                    document.querySelector('.about-content .goods-list').offsetTop > last_known_scroll_position) {
-                    floating_btn.classList.add('show');
-                } else {
-                    floating_btn.classList.remove('show');
-                }
-            }
-
         }
 
         if (lumen_floating_btn) {
@@ -441,25 +419,16 @@ window.requestAnimFrame = (function () {
 
     for (let i = 0; i < buyBtns.length; i++) {
         buyBtns[i].addEventListener('click', () => {
-            if (document.querySelector('.product-list')) {
-                document.querySelector('.product-list').scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                });
-            }
-            if (document.querySelector('.goods-list')) {
-                document.querySelector('.goods-list').scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                });
-            }
-
+            document.querySelector('.product-list').scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
         });
     }
 
     if(document.querySelector('.lumen-intro .intro-content .intro-btn button')) {
         document.querySelector('.lumen-intro .intro-content .intro-btn button').addEventListener('click', () => {
-            document.querySelector('.goods-list-face').scrollIntoView({
+            document.querySelector('.goods-list').scrollIntoView({
                 behavior: 'smooth',
                 block: 'start'
             });
@@ -502,10 +471,8 @@ window.requestAnimFrame = (function () {
         });
     }
 
-    const countdown = document.querySelector('.countdown');
-    const laborCountdown = document.querySelector('.labor-day-title .labor-countdown');
-    const saleCountdown = document.querySelector('.lumen-intro .labor-countdown');
-    const saleTitleCountdown = document.querySelector('.about-intro.count-title.black-sale .btn-wrap .labor-countdown');
+    const countdown = document.querySelector('.countdown')
+    const laborCountdown = document.querySelector('.labor-countdown')
 
     function getTimeRemaining(endtime) {
         const total = Date.parse(endtime) - Date.parse(new Date());
@@ -529,22 +496,21 @@ window.requestAnimFrame = (function () {
         const hoursSpan = clock.querySelector('.giveaway-page .countdown ul li.hours p span');
         const minutesSpan = clock.querySelector('.giveaway-page .countdown ul li.minutes p span');
         const secondsSpan = clock.querySelector('.giveaway-page .countdown ul li.seconds p span');
-        const timeInterval = setInterval(updateClock, 1000);
 
         function updateClock() {
             const t = getTimeRemaining(endtime);
+            daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
-            } else {
-                daysSpan.innerHTML = t.days;
-                hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-                minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-                secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
             }
         }
 
         updateClock();
+        const timeInterval = setInterval(updateClock, 1000);
     }
 
     function initializeLaborClock(item, endtime) {
@@ -553,49 +519,27 @@ window.requestAnimFrame = (function () {
         const hoursSpan = clock.querySelector('ul li.hours p span');
         const minutesSpan = clock.querySelector('ul li.minutes p span');
         const secondsSpan = clock.querySelector('ul li.seconds p span');
-        const timeInterval = setInterval(updateClock, 1000);
-
-        
 
         function updateClock() {
             const t = getTimeRemaining(endtime);
+            daysSpan.innerHTML = t.days;
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
-                if (saleTitleCountdown) {
-                    saleTitleCountdown.classList.add('done');
-                }
-            } else {
-                if (daysSpan) {
-                    daysSpan.innerHTML = t.days;
-                    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-                } else {
-                    // daysSpan.innerHTML = t.days * 24;
-                    hoursSpan.innerHTML = ('0' + (t.hours + t.days * 24)).slice(-2);
-                }
-    
-                minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-                secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
             }
         }
 
         updateClock();
+        const timeInterval = setInterval(updateClock, 1000);
     }
 
     if (laborCountdown) {
         // year-month-dateThours:minutes:seconds:ms-GMT
         const laborDeadline = '2021-09-07T00:00:01.000-00:00';
         initializeLaborClock('.labor-countdown', laborDeadline);
-    }
-    if (saleCountdown) {
-        // year-month-dateThours:minutes:seconds:ms-GMT
-        const saleDeadline = '2021-11-30T10:00:00.000+02:00';
-        initializeLaborClock('.labor-countdown', saleDeadline);
-    }
-    if (saleTitleCountdown) {
-        // year-month-dateThours:minutes:seconds:ms-GMT
-        const saleDeadline = '2021-11-30T10:00:00.000+02:00';
-        initializeLaborClock('.labor-countdown', saleDeadline);
     }
     if (countdown) {
         // year-month-dateThours:minutes:seconds:ms-GMT
@@ -760,292 +704,4 @@ window.requestAnimFrame = (function () {
 
 })();
 
-// Init style shamelessly stolen from jQuery http://jquery.com
-var Froogaloop = (function(){
-    // Define a local copy of Froogaloop
-    function Froogaloop(iframe) {
-        // The Froogaloop object is actually just the init constructor
-        return new Froogaloop.fn.init(iframe);
-    }
-
-    var eventCallbacks = {},
-        hasWindowEvent = false,
-        isReady = false,
-        slice = Array.prototype.slice,
-        playerDomain = '';
-
-    Froogaloop.fn = Froogaloop.prototype = {
-        element: null,
-
-        init: function(iframe) {
-            if (typeof iframe === "string") {
-                iframe = document.getElementById(iframe);
-            }
-
-            this.element = iframe;
-
-            // Register message event listeners
-            playerDomain = getDomainFromUrl(this.element.getAttribute('src'));
-
-            return this;
-        },
-
-        /*
-         * Calls a function to act upon the player.
-         *
-         * @param {string} method The name of the Javascript API method to call. Eg: 'play'.
-         * @param {Array|Function} valueOrCallback params Array of parameters to pass when calling an API method
-         *                                or callback function when the method returns a value.
-         */
-        api: function(method, valueOrCallback) {
-            if (!this.element || !method) {
-                return false;
-            }
-
-            var self = this,
-                element = self.element,
-                target_id = element.id !== '' ? element.id : null,
-                params = !isFunction(valueOrCallback) ? valueOrCallback : null,
-                callback = isFunction(valueOrCallback) ? valueOrCallback : null;
-
-            // Store the callback for get functions
-            if (callback) {
-                storeCallback(method, callback, target_id);
-            }
-
-            postMessage(method, params, element);
-            return self;
-        },
-
-        /*
-         * Registers an event listener and a callback function that gets called when the event fires.
-         *
-         * @param eventName (String): Name of the event to listen for.
-         * @param callback (Function): Function that should be called when the event fires.
-         */
-        addEvent: function(eventName, callback) {
-            if (!this.element) {
-                return false;
-            }
-
-            var self = this,
-                element = self.element,
-                target_id = element.id !== '' ? element.id : null;
-
-
-            storeCallback(eventName, callback, target_id);
-
-            // The ready event is not registered via postMessage. It fires regardless.
-            if (eventName != 'ready') {
-                postMessage('addEventListener', eventName, element);
-            }
-            else if (eventName == 'ready' && isReady) {
-                callback.call(null, target_id);
-            }
-
-            return self;
-        },
-
-        /*
-         * Unregisters an event listener that gets called when the event fires.
-         *
-         * @param eventName (String): Name of the event to stop listening for.
-         */
-        removeEvent: function(eventName) {
-            if (!this.element) {
-                return false;
-            }
-
-            var self = this,
-                element = self.element,
-                target_id = element.id !== '' ? element.id : null,
-                removed = removeCallback(eventName, target_id);
-
-            // The ready event is not registered
-            if (eventName != 'ready' && removed) {
-                postMessage('removeEventListener', eventName, element);
-            }
-        }
-    };
-
-    /**
-     * Handles posting a message to the parent window.
-     *
-     * @param method (String): name of the method to call inside the player. For api calls
-     * this is the name of the api method (api_play or api_pause) while for events this method
-     * is api_addEventListener.
-     * @param params (Object or Array): List of parameters to submit to the method. Can be either
-     * a single param or an array list of parameters.
-     * @param target (HTMLElement): Target iframe to post the message to.
-     */
-    function postMessage(method, params, target) {
-        if (!target.contentWindow.postMessage) {
-            return false;
-        }
-
-        var url = target.getAttribute('src').split('?')[0],
-            data = JSON.stringify({
-                method: method,
-                value: params
-            });
-
-        if (url.substr(0, 2) === '//') {
-            url = window.location.protocol + url;
-        }
-
-        target.contentWindow.postMessage(data, url);
-    }
-
-    /**
-     * Event that fires whenever the window receives a message from its parent
-     * via window.postMessage.
-     */
-    function onMessageReceived(event) {
-        var data, method;
-
-        try {
-            data = JSON.parse(event.data);
-            method = data.event || data.method;
-        }
-        catch(e)  {
-            //fail silently... like a ninja!
-        }
-
-        if (method == 'ready' && !isReady) {
-            isReady = true;
-        }
-
-        // Handles messages from moogaloop only
-        if (event.origin != playerDomain) {
-            return false;
-        }
-
-        var value = data.value,
-            eventData = data.data,
-            target_id = target_id === '' ? null : data.player_id,
-
-            callback = getCallback(method, target_id),
-            params = [];
-
-        if (!callback) {
-            return false;
-        }
-
-        if (value !== undefined) {
-            params.push(value);
-        }
-
-        if (eventData) {
-            params.push(eventData);
-        }
-
-        if (target_id) {
-            params.push(target_id);
-        }
-
-        return params.length > 0 ? callback.apply(null, params) : callback.call();
-    }
-
-
-    /**
-     * Stores submitted callbacks for each iframe being tracked and each
-     * event for that iframe.
-     *
-     * @param eventName (String): Name of the event. Eg. api_onPlay
-     * @param callback (Function): Function that should get executed when the
-     * event is fired.
-     * @param target_id (String) [Optional]: If handling more than one iframe then
-     * it stores the different callbacks for different iframes based on the iframe's
-     * id.
-     */
-    function storeCallback(eventName, callback, target_id) {
-        if (target_id) {
-            if (!eventCallbacks[target_id]) {
-                eventCallbacks[target_id] = {};
-            }
-            eventCallbacks[target_id][eventName] = callback;
-        }
-        else {
-            eventCallbacks[eventName] = callback;
-        }
-    }
-
-    /**
-     * Retrieves stored callbacks.
-     */
-    function getCallback(eventName, target_id) {
-        if (target_id) {
-            return eventCallbacks[target_id][eventName];
-        }
-        else {
-            return eventCallbacks[eventName];
-        }
-    }
-
-    function removeCallback(eventName, target_id) {
-        if (target_id && eventCallbacks[target_id]) {
-            if (!eventCallbacks[target_id][eventName]) {
-                return false;
-            }
-            eventCallbacks[target_id][eventName] = null;
-        }
-        else {
-            if (!eventCallbacks[eventName]) {
-                return false;
-            }
-            eventCallbacks[eventName] = null;
-        }
-
-        return true;
-    }
-
-    /**
-     * Returns a domain's root domain.
-     * Eg. returns http://vimeo.com when http://vimeo.com/channels is sbumitted
-     *
-     * @param url (String): Url to test against.
-     * @return url (String): Root domain of submitted url
-     */
-    function getDomainFromUrl(url) {
-        if (url.substr(0, 2) === '//') {
-            url = window.location.protocol + url;
-        }
-
-        var url_pieces = url.split('/'),
-            domain_str = '';
-
-        for(var i = 0, length = url_pieces.length; i < length; i++) {
-            if(i<3) {domain_str += url_pieces[i];}
-            else {break;}
-            if(i<2) {domain_str += '/';}
-        }
-
-        return domain_str;
-    }
-
-    function isFunction(obj) {
-        return !!(obj && obj.constructor && obj.call && obj.apply);
-    }
-
-    function isArray(obj) {
-        return toString.call(obj) === '[object Array]';
-    }
-
-    // Give the init function the Froogaloop prototype for later instantiation
-    Froogaloop.fn.init.prototype = Froogaloop.fn;
-
-    // Listens for the message event.
-    // W3C
-    if (window.addEventListener) {
-        window.addEventListener('message', onMessageReceived, false);
-    }
-    // IE
-    else {
-        window.attachEvent('onmessage', onMessageReceived);
-    }
-
-    // Expose froogaloop to the global object
-    return (window.Froogaloop = window.$f = Froogaloop);
-
-})();
 //# sourceMappingURL=maps/common.js.map
