1638cd1e68f57298d025c71cd1641b8b5d6fa434
Yotam Faraggi <yotam@curalife.com>
2022-01-12 19:08:28 +0200

    Wed Jan 12 19:08:28 IST 2022

--- assets/redirect-by-location.js
+++ assets/redirect-by-location.js
@@ -27,6 +27,10 @@ if (!isRobot) {
         function n() {
             return e.country
         }
-        window.whereAmIFrom = n, window.location.href.includes("checkout-diabetic") || window.location.href.includes("admin") || window.location.href.includes("parcelpanel") || (o.includes(n()) ? window.location.hostname = "trycuralife.com" : "AT" == n() ? window.location.hostname = "curalife.at" : "US" != n() && "PR" != n() && (window.location.hostname = "global.curalife.com")), "undefined" != typeof geoRules && geoRules()
+
+        if ("AT" == n()) {window.location.hostname = "curalife.at"}
+        else if (o.includes(n())) {window.location.hostname = "trycuralife.com"}
+        else if ("US" != n() && "PR" != n()) {window.location.hostname = "global.curalife.com"}
+        //window.whereAmIFrom = n, window.location.href.includes("checkout-diabetic") || window.location.href.includes("admin") || window.location.href.includes("parcelpanel") || (o.includes(n()) ? window.location.hostname = "trycuralife.com" : "AT" == n() ? window.location.hostname = "curalife.at" : "US" != n() && "PR" != n() && (window.location.hostname = "global.curalife.com")), "undefined" != typeof geoRules && geoRules()
     }, req.send(null);
 }
