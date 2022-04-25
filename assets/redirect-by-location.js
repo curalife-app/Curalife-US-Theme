96ff1714b90a484df96ee794269f3f3207369cc2
Yotam Faraggi <yotam@curalife.com>
2022-04-25 11:39:54 +0300

    Mon Apr 25 11:39:54 IDT 2022

--- assets/redirect-by-location.js
+++ assets/redirect-by-location.js
@@ -21,22 +21,18 @@ if (!isRobot) {
     console.log('REDIRECTING...');
     var req = new XMLHttpRequest;
     req.overrideMimeType("application/json"), req.open("GET", "https://geo.curalife.com/", !0), req.onload = function() {
-        var e = JSON.parse(req.responseText);
+        var country = JSON.parse(req.responseText).country;
         var o = ["KW"];
 
-        function n() {
-            return e.country
-        }
-
-        if ((location.hostname == "curalife.com") && ("US" != n() && "PR" != n())) { // If US and not US or PR so go Global
+        if ((location.hostname == "curalife.com") && (country != "US" && country != "PR")) { // If US and not US or PR so go Global
             window.location.hostname = "global.curalife.com";
             //window.whereAmIFrom = n, window.location.href.includes("checkout-diabetic") || window.location.href.includes("admin") || window.location.href.includes("parcelpanel") || (o.includes(n()) ? window.location.hostname = "trycuralife.com" : "AT" == n() ? window.location.hostname = "curalife.at" : "US" != n() && "PR" != n() && (window.location.hostname = "global.curalife.com")), "undefined" != typeof geoRules && geoRules()
         }
-        else if ((location.hostname == "global.curalife.com") && ("US" === n() || "PR" === n())) // If Global and we are on US or PR so go US
+        else if ((location.hostname == "global.curalife.com") && (country === "US" || country === "PR")) // If Global and we are on US or PR so go US
         {
             window.location.hostname = "curalife.com";
         }
-        else if ("AT" == n()) {window.location.hostname = "curalife.at"} // If Austria
-        else if (o.includes(n())) {window.location.hostname = "trycuralife.com"} // If Kuwait
+        else if (country == "AT") {window.location.hostname = "curalife.at"} // If Austria
+        else if (country == "KW") {window.location.hostname = "trycuralife.com"} // If Kuwait
     }, req.send(null);
 }
