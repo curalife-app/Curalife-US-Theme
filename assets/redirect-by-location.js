b745b9b5f7e0ba9a08c6687f0ade823ba2d888e1
Yotam Faraggi <yotam@curalife.com>
2022-04-25 11:47:25 +0300

    Mon Apr 25 11:47:24 IDT 2022

--- assets/redirect-by-location.js
+++ assets/redirect-by-location.js
@@ -26,7 +26,6 @@ if (!isRobot) {
 
         if ((host == "USA") && (country != "US" && country != "PR")) { // If US and not US or PR so go Global
             window.location.hostname = "global.curalife.com";
-            //window.whereAmIFrom = n, window.location.href.includes("checkout-diabetic") || window.location.href.includes("admin") || window.location.href.includes("parcelpanel") || (o.includes(n()) ? window.location.hostname = "trycuralife.com" : "AT" == n() ? window.location.hostname = "curalife.at" : "US" != n() && "PR" != n() && (window.location.hostname = "global.curalife.com")), "undefined" != typeof geoRules && geoRules()
         }
         else if ((host == "Global") && (country === "US" || country === "PR")) // If Global and we are on US or PR so go US
         {
