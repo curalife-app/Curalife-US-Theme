d28b2bb1e004bc4c4b37c495210e6c16821840dc
Yotam Faraggi <yotam@curalife.com>
2022-04-25 11:46:48 +0300

    Mon Apr 25 11:46:48 IDT 2022

--- assets/redirect-by-location.js
+++ assets/redirect-by-location.js
@@ -19,16 +19,16 @@ const isRobot = detectRobot(userAgent);
 
 if (!isRobot) {
     console.log('REDIRECTING...');
-    var req = new XMLHttpRequest;
+    let req = new XMLHttpRequest;
     req.overrideMimeType("application/json"), req.open("GET", "https://geo.curalife.com/", !0), req.onload = function() {
-        var country = JSON.parse(req.responseText).country;
-        var o = ["KW"];
+        let country = JSON.parse(req.responseText).country;
+        let host = location.hostname == "global.curalife.com" ? "Global" : "USA";
 
-        if ((location.hostname == "curalife.com") && (country != "US" && country != "PR")) { // If US and not US or PR so go Global
+        if ((host == "USA") && (country != "US" && country != "PR")) { // If US and not US or PR so go Global
             window.location.hostname = "global.curalife.com";
             //window.whereAmIFrom = n, window.location.href.includes("checkout-diabetic") || window.location.href.includes("admin") || window.location.href.includes("parcelpanel") || (o.includes(n()) ? window.location.hostname = "trycuralife.com" : "AT" == n() ? window.location.hostname = "curalife.at" : "US" != n() && "PR" != n() && (window.location.hostname = "global.curalife.com")), "undefined" != typeof geoRules && geoRules()
         }
-        else if ((location.hostname == "global.curalife.com") && (country === "US" || country === "PR")) // If Global and we are on US or PR so go US
+        else if ((host == "Global") && (country === "US" || country === "PR")) // If Global and we are on US or PR so go US
         {
             window.location.hostname = "curalife.com";
         }
